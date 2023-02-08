2696266134f57a16b8a950b015fff9e244d07912
TheKany <skarnjs06@naver.com>
2023-02-08 18:46:21 +0900

    pagination 기능 구현

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -6,6 +6,7 @@ import Button from './components/Button';
 import ListItem from './components/ListItem';
 import ListItemLayout from './components/ListItemLayout';
 import Modal from './components/Modal';
+import Pagination from './components/Pagination';
 
 const OpenClosedFilter = ({ size, state, onClick, selected }) => {
   return (
@@ -87,48 +88,60 @@ const ListFilter = ({ onChangeFilter }) => {
 const ListContainer = () => {
   const [inputValue, setInputValue] = useState('is:pr is:open');
   const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const MAX_PAGE = 10;
 
   return (
-    <div className={styles.listContainer}>
-      <div className={styles.topSection}>
-        <input
-          className={styles.input}
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-        />
-        <Button
-          style={{
-            backgroundColor: '#2DA44E',
-            color: 'white',
-            fontSize: '14px',
-          }}
-        >
-          New Issue
-        </Button>
+    <>
+      <div className={styles.listContainer}>
+        <div className={styles.topSection}>
+          <input
+            className={styles.input}
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+          <Button
+            style={{
+              backgroundColor: '#2DA44E',
+              color: 'white',
+              fontSize: '14px',
+            }}
+          >
+            New Issue
+          </Button>
+        </div>
+
+        <OpenClosedFilters />
+
+        <ListItemLayout className={styles.listFilter}>
+          <ListFilter onChangeFilter={(filteredData) => {}} />
+        </ListItemLayout>
+
+        <div className={styles.ListItemcontainer}>
+          {list.map((listItem, idx) => {
+            <ListItem
+              key={idx}
+              badges={[
+                {
+                  bgColor: 'red',
+                  title: 'bug',
+                },
+              ]}
+            />;
+          })}
+        </div>
       </div>
-
-      <OpenClosedFilters />
-
-      <ListItemLayout className={styles.listFilter}>
-        <ListFilter onChangeFilter={(filteredData) => {}} />
-      </ListItemLayout>
-
-      <div className={styles.ListItemcontainer}>
-        {list.map((listItem, idx) => {
-          <ListItem
-            key={idx}
-            badges={[
-              {
-                bgColor: 'red',
-                title: 'bug',
-              },
-            ]}
-          />;
-        })}
-      </div>
-    </div>
+      <Pagination
+        maxPage={MAX_PAGE}
+        currentPage={page}
+        onClickPageButton={(number) => {
+          setPage(number);
+        }}
+      />
+    </>
   );
 };
 
---
+++ src/components/Pagination.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import cx from 'clsx';
+
+import styles from './Pagination.module.css';
+
+const PageButton = ({ number, selected, onClick }) => {
+  return (
+    <button
+      className={cx(styles.button, { [styles.selected]: selected })}
+      onClick={() => onClick(number)}
+    >
+      {number}
+    </button>
+  );
+};
+
+const Pagination = ({ currentPage, maxPage, onClickPageButton }) => {
+  return (
+    <div>
+      <button
+        className={cx(styles.arrowButton, {
+          [styles.disabled]: currentPage === 1,
+        })}
+      >
+        {'< Previous'}
+      </button>
+      {new Array(maxPage).fill(null).map((_, idx) => (
+        <PageButton
+          number={idx + 1}
+          onClick={onClickPageButton}
+          selected={idx + 1 === currentPage}
+        />
+      ))}
+      <button
+        className={cx(styles.arrowButton, {
+          [styles.disabled]: currentPage === maxPage,
+        })}
+      >
+        {'Next >'}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
