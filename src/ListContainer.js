6ac3f17e3338da8c4a787b960f40ca25b58e7308
TheKany <skarnjs06@naver.com>
2023-02-08 11:38:26 +0900

    필터 아이템 부분 컴포넌트 분리 작업

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -6,46 +6,67 @@ import Button from './components/Button';
 import ListItem from './components/ListItem';
 import ListItemLayout from './components/ListItemLayout';
 
+const OpenClosedFilter = ({ size, state, onClick, selected }) => {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
+  );
+};
+
+const OpenClosedFilters = ({ data }) => {
+  const [isOpenMode, setIsOpenMode] = useState(true);
+  // const data = getDate();
+  // const openData = data.filter((data) => data.state === 'open')
+  // const closeData = data.filter((data) => data.state === 'close')
+  const openModeDataSize = 1;
+  const closeModeDataSize = 2;
+
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        selected={isOpenMode}
+        onClick={() => setIsOpenMode(true)}
+      />
+      <OpenClosedFilter
+        size={closeModeDataSize}
+        state="Closed"
+        selected={!isOpenMode}
+        onClick={() => setIsOpenMode(false)}
+      />
+    </>
+  );
+};
+
+const ListFilterItem = ({ onClick, children }) => {
+  return (
+    <span role="button" onClick={onClick}>
+      {children} ▾
+    </span>
+  );
+};
+
+const ListFilter = () => {
+  return (
+    <div className={styles.filterList}>
+      <ListFilterItem>Author</ListFilterItem>
+      <ListFilterItem>Label</ListFilterItem>
+      <ListFilterItem>Projects</ListFilterItem>
+      <ListFilterItem>Milestones</ListFilterItem>
+      <ListFilterItem>Assignee</ListFilterItem>
+      <ListFilterItem>Sort</ListFilterItem>
+    </div>
+  );
+};
+
 const ListContainer = () => {
   const [inputValue, setInputValue] = useState('is:pr is:open');
-
-  const OpenClosedFilter = ({ size, state, onClick, selected }) => {
-    return (
-      <span
-        role="button"
-        className={cx(styles.textFilter, { [styles.selected]: selected })}
-        onClick={onClick}
-      >
-        {size} {state}
-      </span>
-    );
-  };
-
-  const OpenClosedFilters = ({ data }) => {
-    const [isOpenMode, setIsOpenMode] = useState(true);
-    // const data = getDate();
-    // const openData = data.filter((data) => data.state === 'open')
-    // const closeData = data.filter((data) => data.state === 'close')
-    const openModeDataSize = 1;
-    const closeModeDataSize = 2;
-
-    return (
-      <>
-        <OpenClosedFilter
-          size={openModeDataSize}
-          state="Open"
-          selected={isOpenMode}
-          onClick={() => setIsOpenMode(true)}
-        />
-        <OpenClosedFilter
-          size={closeModeDataSize}
-          state="Closed"
-          selected={!isOpenMode}
-          onClick={() => setIsOpenMode(false)}
-        />
-      </>
-    );
-  };
 
   return (
     <div className={styles.listContainer}>
@@ -71,15 +92,9 @@ const ListContainer = () => {
       <OpenClosedFilters />
 
       <ListItemLayout className={styles.listFilter}>
-        <div className={styles.filterList}>
-          <span>Author</span>
-          <span>Label</span>
-          <span>Projects</span>
-          <span>Milestones</span>
-          <span>Assignee</span>
-          <span>Sort</span>
-        </div>
+        <ListFilter />
       </ListItemLayout>
+
       <div className={styles.ListItemcontainer}>
         <ListItem
           badges={[
