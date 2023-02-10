f4be7230856271af93ad4d59ad91fea15d8a425c
TheKany <skarnjs06@naver.com>
2023-02-10 11:11:57 +0900

    ✨ dayjs 사용

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import '../src/styles/base/_reset.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +12,8 @@ root.render(
     <App />
   </React.StrictMode>,
 );
+
+dayjs.extend(relativeTime);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
